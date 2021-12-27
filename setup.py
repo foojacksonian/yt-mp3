b367a9058ea8f7e7661c5051ec355dfaa1f4fd2e
Andrew Michaelis <amac@hyperplane.org>
2021-12-26 23:55:47 -0800

    add setuptools stub

---
+++ setup.py
@@ -0,0 +1,26 @@
+from os import path
+from codecs import open
+from setuptools import setup, find_packages
+
+long_description = "The ytmp3 program downloads, trims, encodes and tags songs acquired from YouTube."
+
+setup(
+    name='yt-mp3',
+    version='0.0.1',
+    description='Simple program to download, trim, encode and tag songs from Youtube.',
+    long_description=long_description,
+    url='https://github.com/foojacksonian/yt-mp3',
+    author='Foo Jacksonian, Andrew Michaelis',
+    author_email='amac@hyperplane.org',
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='music,mp3,YouTube',
+    packages=find_packages(),
+    scripts=['ytmp3.py'],
+    install_requires=['pytube>=11.0.2', 'mutagen>=1.45.1']
+)
